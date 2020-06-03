f5435d7a54243a7803a1e2f6b4df066e358f13a7
mfvargo <mfvargo@gmail.com>
2020-06-03 15:03:59 -0700

    add server code

---
+++ plugins/server/server.h
@@ -0,0 +1,14 @@
+#ifndef JAM_NET_SERVER_H_INCLUDED
+#define JAM_NET_SERVER_H_INCLUDED
+
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../common/jamrtime.h"
+
+#endif // JAM_NET_SERVER_H_INCLUDED
