4817e273a186f9cbedc120bf46cda8d66ccc1259
mfvargo <mfvargo@gmail.com>
2020-10-25 16:12:14 -0700

    use one process and threads to listen to many rooms

--- plugins/server/src/broadcast.cpp
+++ plugins/server/src/broadcast.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <string.h>
 #include "../../common/JamNetStuff.hpp"
+#include <vector>
+#include <thread>
+#include <mutex>
 
-int main(int argc, char **argv) {
+std::vector<std::thread> roomThreads;
 
+void packet_thread(short port) {
   JamNetStuff::JamSocket jamSocket;
   JamNetStuff::JamMixer jamMixer;
-  short port = 7891;
-
-  if (argc > 2 && strcmp(argv[1],"--port") == 0) {
-    port = atoi(argv[2]);
-    printf("port = %s\n", argv[2]);
-  }
-
-
-  std::cout << "Hello World!\n";
-
-  // as the server, bind the socket to port 7891
   jamSocket.initServer(port);
-
   jamSocket.isActivated = true;
+
+  // Space them so the printfs dont interleave
+  std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 68));
 
   // Loop and broadcast data
   time_t now = time(NULL);
@@ -28,10 +23,21 @@ int main(int argc, char **argv) {
     jamSocket.readAndBroadcast(&jamMixer);
     now = time(NULL);
     if (now - lastDump > 3) {
-      printf("port %d: ", port);
-      jamSocket.channelDump();
+      // printf("port %d: ", port);
+      // jamSocket.channelDump();
       lastDump = now;
     }
+  }
+}
+
+int main(int argc, char **argv) {
+  short port = 7891;
+  roomThreads.push_back(std::thread(packet_thread, port++));
+  roomThreads.push_back(std::thread(packet_thread, port++));
+  roomThreads.push_back(std::thread(packet_thread, port++));
+  // Now wait for the thread to exit
+  for (auto & element : roomThreads) {
+    element.join();
   }
   return 0;
 }
