132b5be33f612d63cf829f0ddfbccbda71026852
Mike Vargo <mfvargo@gmail.com>
2020-10-28 22:53:57 -0700

    get rid of thread sleep which was screwing up the first thread.

--- plugins/server/src/broadcast.cpp
+++ plugins/server/src/broadcast.cpp
@@ -8,22 +8,22 @@
 std::vector<std::thread> roomThreads;
 
 void packet_thread(short port) {
+  // Space them so the printfs dont interleave
+  // std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 68));
+
   JamNetStuff::JamSocket jamSocket;
-  JamNetStuff::JamMixer jamMixer;
+  // JamNetStuff::JamMixer jamMixer;
   jamSocket.initServer(port);
   jamSocket.isActivated = true;
 
-  // Space them so the printfs dont interleave
-  std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 68));
 
   // Loop and broadcast data
   time_t now = time(NULL);
   time_t lastDump = time(NULL);
   while (1) {
-    jamSocket.readAndBroadcast(&jamMixer);
+    jamSocket.readAndBroadcast(NULL);
     now = time(NULL);
     if (now - lastDump > 3) {
-      // printf("port %d: ", port);
       // jamSocket.channelDump();
       lastDump = now;
     }
@@ -35,7 +35,8 @@ int main(int argc, char **argv) {
   roomThreads.push_back(std::thread(packet_thread, port++));
   roomThreads.push_back(std::thread(packet_thread, port++));
   roomThreads.push_back(std::thread(packet_thread, port++));
-  // Now wait for the thread to exit
+  roomThreads.push_back(std::thread(packet_thread, port++));
+  // Now wait for the threads to exit
   for (auto & element : roomThreads) {
     element.join();
   }
