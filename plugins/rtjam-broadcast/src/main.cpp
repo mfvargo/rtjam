4296f1277a51468b7f81ea0efded09b6f8626bf4
Mike Vargo <mfvargo@gmail.com>
2021-04-18 07:21:31 +0100

    set use broadcastUnit name for rooms.

--- plugins/rtjam-broadcast/src/main.cpp
+++ plugins/rtjam-broadcast/src/main.cpp
@@ -16,7 +16,6 @@ vector<thread> roomThreads;
 #define FIFO_FRAME_SIZE 960
 
 void packet_thread(short port, json room) {
-  clog << room.dump(2) << endl;
   JamNetStuff::JamSocket jamSocket;
   jamSocket.initServer(port);
   jamSocket.isActivated = true;
@@ -31,22 +30,22 @@ int main(int argc, char **argv) {
   settings.loadFromFile();
   string urlBase = settings.getOrSetValue("rtjam-nation", std::string("http://rtjam-nation.basscleftech.com/api/1/"));
   string token = "";
+  string broadcastUnitName = "";
   settings.saveToFile();
   RTJamNationApi api(urlBase);
   
   while (token == "") {
-    clog << "waiting to checkin with the nation" << endl;
     if (api.broadcastUnitDeviceRegister() && api.m_httpResponseCode == 200) {
       // get the token
       token = api.m_resultBody["broadcastUnit"]["token"];
-      clog << "got a token: " << token << endl;
+      broadcastUnitName = api.m_resultBody["broadcastUnit"]["name"];
     }
     sleep(5);
   }
 
   for (short port = 7891; port < 7894; port++) {
     char roomName[100];
-    sprintf(roomName, "Room: %d", port);
+    sprintf(roomName, "%s:%d", broadcastUnitName.c_str(), port);
     api.activateRoom(token, roomName, port);
     // Start up the room thread with their attributes
     roomThreads.push_back(std::thread(packet_thread, port, api.m_resultBody["room"]));
