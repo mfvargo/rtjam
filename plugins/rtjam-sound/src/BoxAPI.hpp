0cd75b653181714b42265e3e4deff5a46ecb4357
Mike Vargo <mfvargo@gmail.com>
2021-07-16 08:53:45 +0100

    put back the command execute

--- plugins/rtjam-sound/src/BoxAPI.hpp
+++ plugins/rtjam-sound/src/BoxAPI.hpp
@@ -149,19 +149,18 @@ private:
 
     string execMyCommand(string cmd)
     {
-        return "bob";
-        // array<char, 128> buffer;
-        // string result;
-        // unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
-        // if (!pipe)
-        // {
-        //     throw std::runtime_error("popen() failed!");
-        // }
-        // while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-        // {
-        //     result += buffer.data();
-        // }
-        // return result;
+        array<char, 256> buffer;
+        string result;
+        unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+        if (!pipe)
+        {
+            throw std::runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+        {
+            result += buffer.data();
+        }
+        return result;
     }
 
     void getParamForm()
