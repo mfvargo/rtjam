fefd95b40e2dbc5bd8d1a750b73fa88e04316562
Mike Vargo <mfvargo@gmail.com>
2021-07-24 06:39:45 +0100

    tweak the tone knob a bit

--- plugins/common/Distortion.hpp
+++ plugins/common/Distortion.hpp
@@ -24,9 +24,9 @@ public:
 
     // Now setup the settings this effect can receive.
     EffectSetting setting;
-    
+
     setting.init(
-        "drive1",                  // Name
+        "drive1",                 // Name
         EffectSetting::floatType, // Type of setting
         0.0,                      // Min value
         40.0,                     // Max value
@@ -34,10 +34,9 @@ public:
         EffectSetting::dB);
     setting.setFloatValue(6.0);
     m_settingMap.insert(std::pair<std::string, EffectSetting>(setting.name(), setting));
-    
-    
+
     setting.init(
-        "drive2",                  // Name
+        "drive2",                 // Name
         EffectSetting::floatType, // Type of setting
         0.0,                      // Min value
         40.0,                     // Max value
@@ -45,9 +44,7 @@ public:
         EffectSetting::dB);
     setting.setFloatValue(0.0);
     m_settingMap.insert(std::pair<std::string, EffectSetting>(setting.name(), setting));
-    
-    
-    
+
     setting.init(
         "clipType",             // Name
         EffectSetting::intType, // Type of setting
@@ -58,39 +55,37 @@ public:
     setting.setLabels({"hard", "soft", "asym", "even"});
     setting.setIntValue(ClipType::soft);
     m_settingMap.insert(std::pair<std::string, EffectSetting>(setting.name(), setting));
-    
 
-  setting.init(
-        "tone",                  // Name
+    setting.init(
+        "tone",                   // Name
         EffectSetting::floatType, // Type of setting
-        -1.0,                      // Min value
-        1.0,                     // Max value
-        0.05,                      // Step Size
-        EffectSetting::dB);
-    setting.setFloatValue(6.0);
+        -1.0,                     // Min value
+        1.0,                      // Max value
+        0.05,                     // Step Size
+        EffectSetting::linear);
+    setting.setFloatValue(0.0);
     m_settingMap.insert(std::pair<std::string, EffectSetting>(setting.name(), setting));
-    
 
     setting.init(
         "level",                  // Name
         EffectSetting::floatType, // Type of setting
         -60.0,                    // Min value
-        6.0,                     // Max value
+        6.0,                      // Max value
         0.5,                      // Step Size
         EffectSetting::dB);
     setting.setFloatValue(0.0);
     m_settingMap.insert(std::pair<std::string, EffectSetting>(setting.name(), setting));
 
-    // pre-and post-distortion filters (fixed) 
+    // pre-and post-distortion filters (fixed)
     m_hpfFreq = 200;
     m_lpf1Freq = 5000;
     m_lpf2Freq = 5000;
 
-    // tone filters used in crossFade tone control 
+    // tone filters used in crossFade tone control
     // fixed but cross-fading between the two changes tone between
     // "warm" and "bright"
-    m_toneLpfFreq = 6200;
-    m_toneHpfFreq = 5000;
+    m_toneLpfFreq = 2000;
+    m_toneHpfFreq = 3000;
 
     loadFromConfig();
   };
@@ -125,7 +120,6 @@ public:
       m_tone = it->second.getFloatValue();
     }
 
-
     it = m_settingMap.find("level");
     if (it != m_settingMap.end())
     {
@@ -141,21 +135,19 @@ public:
       // HPF in chain - determines amount of low-freqs sent to clipper block
       // use F~140 Hz for full-range. F=720 for Tubescreamer type distorion
       float value = m_hpf.getSample(input[i]);
-      
+
       // Stage 1 - first clipper (set to emulate op-amp clipping before diodes)
       value = clipSample(value * m_gain1); // clip signal
-      value = m_lpf1.getSample(value);   // filter out higher-order harmonics
+      value = m_lpf1.getSample(value);     // filter out higher-order harmonics
 
-      // Stage 2 - diode clipper 
-      value = clipSample(value * m_gain2) * m_level;
-      value = m_lpf2.getSample(value);   // filter out higher-order harmonics
-      
+      // Stage 2 - diode clipper
+      value = clipSample(value * m_gain2);
+      value = m_lpf2.getSample(value); // filter out higher-order harmonics
+
       // Stage 3 - Tone control
       // simple tone control that cross-fades between low and high-pass filters
       //
-      float m_toneLpfOut = m_toneLpf.getSample(value);
-      float m_toneHpfOut = m_toneHpf.getSample(value);
-      value = SignalBlock::crossFade(m_toneLpfOut, m_toneHpfOut, m_tone); 
+      value = SignalBlock::crossFade(m_toneHpf.getSample(value), m_toneLpf.getSample(value), m_tone);
 
       // value = distortionAlgorithm(value);
       output[i] = value * m_level;
@@ -163,33 +155,27 @@ public:
   };
 
 private:
-  
-  BiQuadFilter m_hpf;        // runs at 48k on input signal
-  BiQuadFilter m_lpf1;        // runs at 48k on clip1 output signal
-  BiQuadFilter m_lpf2;        // runs at 48k on clip1 output signal
-
+  BiQuadFilter m_hpf;  // runs at 48k on input signal
+  BiQuadFilter m_lpf1; // runs at 48k on clip1 output signal
+  BiQuadFilter m_lpf2; // runs at 48k on clip1 output signal
 
   BiQuadFilter m_upsample;   // runs at 8 * 48k after upsample
   BiQuadFilter m_downsample; // runs at 8 * 48k after clip and before downsample
 
-  BiQuadFilter m_toneLpf;   
-  BiQuadFilter m_toneHpf; 
-
-  float m_toneLpfOut;
-  float m_toneHpfOut;
+  BiQuadFilter m_toneLpf;
+  BiQuadFilter m_toneHpf;
 
   // Parameters
-  float m_gain1;        // gain before clip functions
-  float m_gain2;        // gain before clip functions
-  float m_tone;        // tone control 
+  float m_gain1;       // gain before clip functions
+  float m_gain2;       // gain before clip functions
+  float m_tone;        // tone control
   float m_level;       // Overall level
   ClipType m_clipType; // what kind of clipping funciton
-  float m_lpf1Freq;     // frequency of the first clip block lpf (fixed filter)
-  float m_lpf2Freq;     // frequency of the first clip block lpf (fixed filter)
+  float m_lpf1Freq;    // frequency of the first clip block lpf (fixed filter)
+  float m_lpf2Freq;    // frequency of the first clip block lpf (fixed filter)
   float m_hpfFreq;     // frequency of the hpf before first clip block (fixed filter)
   float m_toneLpfFreq; // LPF cut-off frequency for tone control
   float m_toneHpfFreq; // HPF cut-off frequency for tone control
-  
 
   void setupFilters()
   {
@@ -201,8 +187,6 @@ private:
     m_downsample.init(BiQuadFilter::FilterType::HighPass, 48000, 1.0, 1.0, 8 * 48000);
     m_toneLpf.init(BiQuadFilter::FilterType::LowPass, m_toneLpfFreq, 1.0, 1.0, 48000);
     m_toneHpf.init(BiQuadFilter::FilterType::HighPass, m_toneHpfFreq, 1.0, 1.0, 48000);
-  
-
   };
 
   float distortionAlgorithm(float inputSample)
@@ -236,13 +220,13 @@ private:
       filterOut[i] = 8 * m_gain1 * filterOut[i];
       clipOut[i] = clipSample(filterOut[i]);
     }
-    
-    // filter at upsampled Fs/8 before downsampling 
+
+    // filter at upsampled Fs/8 before downsampling
     for (i = 0; i < 8; i++)
     {
       clipOut[i] = m_downsample.getSample(clipOut[i]);
     }
-    
+
     // down-sample the vector to back native sample rate
     return clipOut[0];
   };
--- plugins/common/SignalBlock.hpp
+++ plugins/common/SignalBlock.hpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include "json.hpp"
+#include <iostream>
 
 using json = nlohmann::json;
 
@@ -10,7 +11,7 @@ class SignalBlock
 public:
   virtual float getSample(float input) = 0;
   virtual json getConfig() = 0;
-  
+
   void getBlock(const float *input, float *output, int framesize)
   {
     for (int i = 0; i < framesize; i++)
@@ -18,7 +19,7 @@ public:
       output[i] = getSample(input[i]);
     }
   }
-  
+
   static float dbToFloat(float valueInDB)
   {
     if (valueInDB < -59.5)
@@ -27,7 +28,7 @@ public:
     }
     return std::exp((valueInDB / 72.0f) * 72.0f / 8.656170245f);
   }
-  
+
   static float FloatToDb(float value)
   {
     return (20 * std::log10(value));
@@ -35,19 +36,18 @@ public:
 
   static float crossFade(float input1, float input2, float crossFade)
   {
-    
+
     // Equal power crossfade
     // returns float values representing the volumes of the left and right channels
     // when crossFade = -1, gain1 = 0, gain2 = 1
     // when crossFade = 0, gain1 = 0.7071, gain2 = 0.7071
     // when crossFade = 1, gain1 = 1, gain2  = 0
 
-      float m_gain1 = sqrt(0.5f * (1.0f + crossFade));
-      float m_gain2 = sqrt(0.5f * (1.0f - crossFade));
+    float gain1 = sqrt(0.5f * (1.0f + crossFade));
+    float gain2 = sqrt(0.5f * (1.0f - crossFade));
+    // std::cout << gain1 << std::endl;
+    float value = input1 * gain1 + input2 * gain2;
 
-      float value = input1*m_gain1 + input2*m_gain2;
-
-      return value;
+    return value;
   };
-
 };
