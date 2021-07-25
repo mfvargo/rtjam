143f90eefe00f5ad92b1a7d295a8940120020d5d
darius61 <darius@dsp-fx.com>
2021-07-25 04:24:01 +0100

    Cleanup, add codec control code to status poll loop

--- plugins/rtjam-status/src/main.cpp
+++ plugins/rtjam-status/src/main.cpp
@@ -1,5 +1,3 @@
-#if 1 
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,6 +7,54 @@
 #include "LightData.hpp"
 #include "CodecControl.hpp"
 
+
+using namespace std;
+
+#if 1
+int main(int argc, char *argv[])
+{
+    LightData lightData;
+    StatusLight::startInit();
+    StatusLight status, inputOne, inputTwo;
+    status.init(StatusLight::status);
+    inputOne.init(StatusLight::inputOne);
+    inputTwo.init(StatusLight::inputTwo);
+
+    lightData.m_pLightSettings->inputOne = green;
+    lightData.m_pLightSettings->inputTwo = green;
+    lightData.m_pLightSettings->status = green;
+
+    int i = 0;
+    int delay = 10000;
+
+
+    // Codec init and gain/volume control
+    CodecControlAndStatus codecControl;
+    codecControl.init();
+
+
+
+
+    while (1)
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(delay));
+        status.set(lightData.m_pLightSettings->status);
+        inputOne.set(lightData.m_pLightSettings->inputOne);
+        inputTwo.set(lightData.m_pLightSettings->inputTwo);
+    
+
+        // TODO - test
+        codecControl.updateVolumes();
+
+
+    };
+    return 0;
+}
+
+
+#else 
+
+// Test code only
 
 int main(int argc, char *argv[])
 {
@@ -41,76 +87,6 @@ int main(int argc, char *argv[])
         delayMicroseconds(1000000);
     };
     return 0;
-}
-#else
-
-
-#include <stdio.h>
-#include <fcntl.h>			// O_RDWR
-#include <stdlib.h>			// exit()
-#include <sys/ioctl.h>		// ioctl()
-#include <linux/i2c-dev.h>	// I2C_SLAVE
-#include <unistd.h>			// read()
-#include <errno.h>			// errno
-
-int main(int argc, char **argv)
-{	
-	int file;
-	char *filename = "/dev/i2c-1";
-	if ((file = open(filename, O_RDWR)) < 0)
-	{
-		perror("Failed to open i2c bus");
-		exit(-1);
-	}
-	
-	int addr = 0b0101001;
-	if (ioctl(file, I2C_SLAVE, addr) < 0)
-	{
-		perror("Failed to access i2c bus");
-		exit(-1);
-	}
-	
-while(1)
-{
-
-	char cmd[1] = {0};
-	cmd[0] = 0b11110100;		// D7 to D4 : CH3 to CH0
-								// D3 : REF_SEL (default 0)
-								// D2 : FLTR (default 1)
-								// D1 : Bit trial delay (default 0)
-								// D0 : Sample delay (default 0)
-								
-	short debug_write = 1;
-	if (debug_write == 1)
-	{
-		if (write(file, cmd, 1) != 1)
-		{
-			printf("Failed to write to i2c bus\n\n");
-		}
-	}
-	
-	char buf[10] = {0};
-	float data;
-	char channel;
-	int i;
-	for (i = 0; i < 4; i++)
-	{
-		if (read(file, buf, 2) != 2)
-		{
-			printf("Failed to read from i2c bus\n\n");
-		}
-		else {
-			channel = ((buf[0] & 0b00110000) >> 4);
-			data = (float)((buf[0] & 0x0F) << 8) + buf[1];
-			//10-bit range for AD7991
-			//data = data / 4095;
-			printf("Channel %02d Data: %04f\n", channel + 1, data);
-		}
-	}
-
-}
-
-	return 0;
 }
 
 #endif
