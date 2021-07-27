d0b1192c218146dc644ed3de675b18f7c29040e8
darius61 <darius@dsp-fx.com>
2021-07-27 16:08:17 +0100

    check in main - added hardware detect and polling for new board

--- plugins/rtjam-status/src/main.cpp
+++ plugins/rtjam-status/src/main.cpp
@@ -7,12 +7,12 @@
 #include "LightData.hpp"
 #include "CodecControl.hpp"
 
-
 using namespace std;
 
-#if 1
 int main(int argc, char *argv[])
 {
+ 
+ 
     LightData lightData;
     StatusLight::startInit();
     StatusLight status, inputOne, inputTwo;
@@ -24,17 +24,30 @@ int main(int argc, char *argv[])
     lightData.m_pLightSettings->inputTwo = green;
     lightData.m_pLightSettings->status = green;
 
-    int i = 0;
-    int delay = 10000;
-
+    int rtjamHardwareDetected = 0;
 
     // Codec init and gain/volume control
     CodecControlAndStatus codecControl;
-    codecControl.init();
+ 
 
+    int i = 0;
+    int delay = 10000;
 
+    
+    // init codec - function inits hardware and returns 1 if custom hardware detected 
+    if(codecControl.init() == 1)
+    {
+        rtjamHardwareDetected = 1;  // rtjam hardware detected
+    }
+    else
+    {
+        rtjamHardwareDetected = 0;  // standard Rpi setup
 
+    };
 
+    // main status poll loop
+    //  - updates LEDs
+    //   reads pots, update codec gain control registers if custom board detected
     while (1)
     {
         std::this_thread::sleep_for(std::chrono::microseconds(delay));
@@ -42,51 +55,12 @@ int main(int argc, char *argv[])
         inputOne.set(lightData.m_pLightSettings->inputOne);
         inputTwo.set(lightData.m_pLightSettings->inputTwo);
     
-
-        // TODO - test
-        codecControl.updateVolumes();
-
-
-    };
+        // poll the hardware if custom board detected
+        if(rtjamHardwareDetected == 1)
+        {
+            codecControl.updateVolumes();
+        }
+        
+    }
     return 0;
 }
-
-
-#else 
-
-// Test code only
-
-int main(int argc, char *argv[])
-{
-    
-    // create instance to test control code
-    
-    CodecControlAndStatus codecControl;
-    codecControl.init();
-
-    // set up LEDs pins and turn on all lights at startup
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(23, OUTPUT);
-    pinMode(24, OUTPUT);
-    
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(23, HIGH);
-    digitalWrite(24, HIGH);
-    
-    // loop forever (will print out raw ADC and gain register values to console)
-    while (1)
-    {
-
-        codecControl.updateVolumes();
-        delayMicroseconds(1000000);
-    };
-    return 0;
-}
-
-#endif
