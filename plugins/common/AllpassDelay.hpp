9b826ed8516caa4c024764d3ae90e491ab27583f
Mike Vargo <mfvargo@gmail.com>
2021-08-09 19:31:21 +0100

    move member to stack variables.  Simplify index wrapping.

--- plugins/common/AllpassDelay.hpp
+++ plugins/common/AllpassDelay.hpp
@@ -3,55 +3,33 @@
 #include "SignalBlock.hpp"
 #include <vector>
 
-
 class AllpassDelay : public SignalBlock
 {
 public:
-  
+    void init(float delayLength, int apGain)
+    {
+        // initialize the delay line and coeffs
+        m_apGain = apGain;
+        m_apDelayIndex = 0;
+        m_apDelayLength = delayLength;
+        m_apDelayLine.resize(m_apDelayLength);
+    };
 
-  void init(float delayLength, int apGain)
-  {
-      
-      // initialize the delay line and coeffs
-      m_apGain = apGain;
-      m_apDelayIndex = 0;
-      
-      m_apDelayLength = delayLength;
-      m_apDelayLine.resize(m_apDelayLength);
-      
-  };
-
-
-  float getSample(float input) override
-  {
-    
-
-        // all-pass delay or "stretched" all-pass 
-
-        m_apDelayOut = m_apDelayLine[m_apDelayIndex]; 	// read in end of delay line  
-		m_apDelayIn = input + m_apDelayOut*m_apGain;    // delay in - sum of input and fb path   
-	    m_apDelayLine[m_apDelayIndex] = m_apDelayIn;	    // write to delay line - new delay sample  
-		m_apDelayOut = m_apDelayOut + m_apDelayIn*(-1.0*m_apGain);	// ap out = sum of delay out and ff path  
-
-    	if(m_apDelayIndex++ > m_apDelayLength)
-    	{
-    	    m_apDelayIndex = 0;
-    	}
-
-
-      return m_apDelayOut;
-  
-  };
+    float getSample(float input) override
+    {
+        // all-pass delay or "stretched" all-pass
+        float delayOut = m_apDelayLine[m_apDelayIndex]; // read in end of delay line
+        float delayIn = input + delayOut * m_apGain;    // delay in - sum of input and fb path
+        m_apDelayLine[m_apDelayIndex] = delayIn;        // write to delay line - new delay sample
+        delayOut += delayIn * (-1.0 * m_apGain);        // ap out = sum of delay out and ff path
+        ++m_apDelayIndex %= m_apDelayLength;            // wrap the index around the buffer
+        return delayOut;
+    };
 
 public:
-  
-  
-
 private:
-    float m_apGain, m_apDelayIn, m_apDelayOut;
-    
+    float m_apGain;
     std::vector<float> m_apDelayLine;
     int m_apDelayLength;
     int m_apDelayIndex;
-
 };
