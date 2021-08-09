868d0c852c043e0c90dac365d86efc73bbfdf490
darius61 <darius@dsp-fx.com>
2021-08-09 23:54:57 +0100

    reverb bugfix/tweaks - fixed bug with time, changed delays for 48ksps, add low and high cut controls

--- plugins/common/SigmaReverb.hpp
+++ plugins/common/SigmaReverb.hpp
@@ -35,18 +35,39 @@ public:
     setting.setFloatValue(0.01);
     addSetting(setting);
 
+    setting.init(
+        "lowFreq",                 // Name
+        EffectSetting::floatType, // Type of setting
+        10,                       // Min value
+        250,                      // Max value
+        5,                        // Step Size
+        EffectSetting::linear);
+    setting.setFloatValue(10.0);
+    addSetting(setting);
+
+setting.init(
+        "highFreq",                 // Name
+        EffectSetting::floatType, // Type of setting
+        400,                       // Min value
+        8000,                      // Max value
+        5,                        // Step Size
+        EffectSetting::linear);
+    setting.setFloatValue(8000);
+    addSetting(setting);
+
+
     loadFromConfig();
 
     // init the attack/release signal block
     //
-    m_lap1.init(457, 0.6); // ap params
-    m_lap2.init(653, 0.6);
-    m_lap3.init(973, 0.6);
-    m_lap4.init(1605, 0.6);
-    m_ap1.init(3665, 0.6);
-    m_ap1b.init(5130, 0.6);
-    m_ap2.init(4228, 0.6);
-    m_ap2b.init(3935, 0.6);
+    m_lap1.init(229, 0.6); // ap params
+    m_lap2.init(321, 0.6);
+    m_lap3.init(471, 0.6);
+    m_lap4.init(803, 0.6);
+    m_ap1.init(1821, 0.6);
+    m_ap1b.init(2565, 0.6);
+    m_ap2.init(2114, 0.6);
+    m_ap2b.init(1817, 0.6);
   };
 
   void loadFromConfig() override
@@ -56,6 +77,12 @@ public:
 
     m_reverbTime = getSettingByName("time").getFloatValue();
     m_reverbLevel = getSettingByName("level").getFloatValue();
+    m_lowFreqCut = getSettingByName("lowFreq").getFloatValue();
+    m_highFreqCut = getSettingByName("highFreq").getFloatValue();
+
+    m_lfFilter.init(BiQuadFilter::FilterType::HighPass, m_lowFreqCut, 1.0, 1.0, 48000);
+    m_hfFilter.init(BiQuadFilter::FilterType::LowPass, m_highFreqCut, 1.0, 1.0, 48000);
+
   };
 
   //  TODO - add block diagram here...
@@ -67,7 +94,7 @@ public:
     //
     for (int sample = 0; sample < framesize; sample++)
     {
-      float reverbIn = 0.2 * input[sample]; // scale by .2 to match FV-1 impementations
+      float reverbIn = 1.0 * input[sample]; // scale by .2 to match FV-1 impementations
 
       // read samples from end of each delay line
       float delay1Out = m_delay1[m_delay1Index];
@@ -81,18 +108,17 @@ public:
       value = m_lap4.getSample(value);
 
       float sum1Out = value + (delay2Out * m_reverbTime);
-      //float value = input[sample] + (delay2Out * m_reverbTime);
-
+      
       // process stretched all-pass Chain 2 - AP1->AP1B
       value = m_ap1.getSample(sum1Out);
       value = m_ap1b.getSample(value);
 
       // write result to delay line 1
       m_delay1[m_delay1Index] = value;
-      ++m_delay1Index %= 10000;
+      ++m_delay1Index %= 4853;
 
       // read from end of delay line 1
-      value = delay1Out * m_reverbLevel;
+      value = delay1Out * m_reverbTime;
 
       // process streched all-pass Chain 3 - AP2->AP2B
       // input to Chain 3 is delay 1 output * reverb time
@@ -100,10 +126,12 @@ public:
       value = m_ap2b.getSample(value);
 
       // TODO - add lpf/hpf here
+      value = m_lfFilter.getSample(value);
+      value = m_hfFilter.getSample(value);
 
       // write result to delay line 2
       m_delay2[m_delay2Index] = value;
-      ++m_delay2Index %= 12000;
+      ++m_delay2Index %= 5888;
 
       // reverb output = sum of two delay taps
       value = delay1Out + delay2Out;
@@ -117,12 +145,19 @@ private:
   AllpassDelay m_lap1, m_lap2, m_lap3, m_lap4, m_ap1, m_ap1b; // allpass filter chain 1
   AllpassDelay m_ap2, m_ap2b;                                 // allpass filter chain 2
 
-  float m_delay1[10000]; // delay line 1
+  BiQuadFilter m_lfFilter, m_hfFilter;
+
+  float m_lowFreqCut;    // reverb low freq cutoff
+  float m_highFreqCut;   // reverb ring high freq cutoff (damping)
+
+
+  float m_delay1[4853]; // delay line 1
   unsigned int m_delay1Index;
 
-  float m_delay2[12000]; // delay line 2
+  float m_delay2[5888]; // delay line 2
   unsigned int m_delay2Index;
 
   float m_reverbTime;  // reverb time - 0-1 = 0-inf
   float m_reverbLevel; // output level (dry + reverb * 0-1)
+
 };
