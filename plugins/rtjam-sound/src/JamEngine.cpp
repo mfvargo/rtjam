a7eeb60f72e001f60bbc39d6d439d63d07a6b03c
Mike Vargo <mfvargo@gmail.com>
2022-01-25 14:24:11 -0800

    get rid of disconnec timer

--- plugins/rtjam-sound/src/JamEngine.cpp
+++ plugins/rtjam-sound/src/JamEngine.cpp
@@ -246,7 +246,8 @@ void JamEngine::run(const float **inputs, float **outputs, uint32_t frames)
   {
     // It's been 60 minutes since we connected or we got a paramConnectionKeepAlive
     // Time to disconnect
-    disconnect();
+    // TODO: Put this back in once I figure why it's not working correctly.
+    // disconnect();
   }
 
   m_framecount += frames;
